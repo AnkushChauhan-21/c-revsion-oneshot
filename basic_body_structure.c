1d2a721d24a9299eb7267cec7eed3e6ddc9fbfbb
Ankush Chauhan <ankush2026chauhan@gmail.com>
2026-09-04 11:35:21 +0530

    basic programs

---
+++ basic_body_structure.c
@@ -0,0 +1,21 @@
+# include <stdio.h> // This line tells the compiler to include the standard input-output library, which allows us to use functions like printf() for displaying output on the screen.
+int main() {
+  printf("Hello,World!!\n"); // This line prints the string "Hello,World!!" followed by a new line character to the console.
+   return 0; // This line indicates that the program has completed successfully and returns 0 to the operating system.
+}
+
+
+// This is the basic body of the c langauge we mainly use to write our code. The main function is the entry point of the program, and it is where the execution of the program begins.
+
+
+
+// This is a single line comment in C. It is used to add notes or explanations within the code, which are ignored by the compiler during execution. 
+
+
+/*
+
+This is a 
+multiline comment in C. It can span multiple lines and is also ignored by the compiler. Multiline comments are useful for providing detailed explanations or documentation within the code.
+
+
+*/
---
+++ datatypes/data_types.c
@@ -0,0 +1,19 @@
+// lets talk about some data types in C programming language.
+
+//int ,float,char('char',"string")
+
+//before we try a code with different program
+// lets learn how to take and give input and output in C programming Language.
+
+// gcc file name -o file name without.c
+// \filename.exe
+# include <stdio.h>
+int main(){
+  int a ;
+  printf("Enter the number here : \n",a);
+  scanf("%d",&a);
+  printf("The Number is %d",a);
+  return 0;
+
+
+}
