be45ed7f152f033ad366248dc0fb883312d12e1c
Ankush Chauhan <ankush2026chauhan@gmail.com>
2026-09-04 14:19:11 +0530

    done

---
+++ variables/constant.c
@@ -0,0 +1,15 @@
+// constant values are those which cannot be changed after declaring them as a consntant value and it will throw an error if we try to change it later::
+
+// Test code
+# include <stdio.h>
+int main(){
+    const a = 45;
+    printf("The value of the const a is :: %d \n",a);
+
+    // a = 34;
+    // printf("The new value of the a is changed or will it throw an error :: %d",a)
+
+
+
+
+}
---
+++ variables/variables.c
@@ -0,0 +1,16 @@
+// Rules for naming the varaiables are:
+// 1) the name can only start from charcter lower , upper and underscore_
+// 2) name cannot start from the number and any speacial characters
+
+// lets build one code for that
+
+# include <stdio.h>
+void main(){
+int variable1 = 34;
+char _char_varaiable = 'h';
+char string_variable[]="Hello";
+
+printf("The int value of the variable1 is : %d \n",variable1);
+printf("The char value for the _char_variable is : %c \n",_char_varaiable);
+printf("The string value for the array string_variable is : %s \n",string_variable);
+}
