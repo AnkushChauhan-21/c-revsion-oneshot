46d313caceed3625334c6767933115ede6b87eb8
Ankush Chauhan <ankush2026chauhan@gmail.com>
2026-09-06 21:04:44 +0530

    string and its functons

--- string/strings.c
+++ string/strings.c
@@ -9,6 +9,16 @@ int main(){
     printf("Our string is (%s)\n",my_string);
     strcpy(str1,my_string);
     printf("Our string is (%s)\n",str1);
-
+    strcpy(str1,"I am a copy");
+    printf("Our string is (%s)\n",str1);
+    strcpy(str2,"i am a addition");
+    printf("Our string is (%s)\n",str2);
+    strcat(str1,str2);
+    printf("Our string is (%s)\n",str1);
+   
+    printf("%d",strcmp(str1,str2)); // if str1 == str2 it returns the 0
+    // if str1 > str2 it returns 1
+    //  if str1 < str2 it returns the -1
+    // it checks the  alphabet numbers suppose a anb  z is greator then the a ,,so z id greater
     return 0;
 }
