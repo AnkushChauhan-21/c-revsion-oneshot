d663521173ddb44be04a4fa9f9a1e36221033b6a
Ankush Chauhan <ankush2026chauhan@gmail.com>
2026-09-06 09:24:31 +0530

    done with operators

---
+++ operators/bitwise_operators.c
@@ -0,0 +1,19 @@
+# include<stdio.h>
+int main(){
+    printf("\n---- Lets explore some bitwise operators-----\n");
+ int A = 60 , B=14;
+
+
+    printf("The and Bitwise operator result is %d\n", A&B);
+    printf("The or Bitwise operator result is %d\n",A|B);
+    printf("The xor Bitwise operator result is %d\n",A^B);
+    printf("The ones Bitwise operator result is %d\n",~B);
+    printf("The left shift Bitwise operator result is %d\n",A<<B);
+    printf("The right shift Bitwise operator result is %d\n",A>>B);
+
+
+  return 0;
+}
+
+
+// This topic need further explorationa as these are not some normal operators
---
+++ operators/logical_operators.c
@@ -0,0 +1,15 @@
+# include<stdio.h>
+int main(){
+    printf("\n ----Lets try some logical operators\n---");
+    int h=0 , j=1;
+
+    // Lets check some logical operators here
+    printf("The and logical operator result is %d\n",h && j);
+    printf("The or logical operator result is %d\n",h || j);
+    printf("The not logical operator result is %d\n",!j);
+    printf("The not logical operator result is %d\n",!h);
+
+    return 0;
+
+
+}
