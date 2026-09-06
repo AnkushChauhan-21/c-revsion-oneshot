dea3b8de2c7ee90c24090bb314d5df9664500284
Ankush Chauhan <ankush2026chauhan@gmail.com>
2026-09-06 12:21:19 +0530

    while loop done

---
+++ Loops/while.c
@@ -0,0 +1,14 @@
+// lets do while loop with an simple example
+#include <stdio.h>
+int main()
+{
+
+    int index = 0;
+    while (index <= 10)
+    {
+        printf("%d\n\n", index);
+        index++;
+    }
+
+    return 0;
+}
