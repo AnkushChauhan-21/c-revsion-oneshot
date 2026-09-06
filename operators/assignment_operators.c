5606746a89bebab1afbb2a2a9da8ed3bb984b772
Ankush Chauhan <ankush2026chauhan@gmail.com>
2026-09-06 09:36:24 +0530

    done with operators

---
+++ operators/assignment_operators.c
@@ -0,0 +1,18 @@
+# include<stdio.h>
+int main(){
+    printf("\n-------- Lets try somke assignment operators--------\n");
+ int a = 10;
+ a += 10;
+ printf("\nThe += operator is %d\n", a);
+ a -= 10;
+ printf("\nThe -= operator is %d\n", a);
+ a *= 10;
+ printf("\nThe *= operator is %d\n", a);
+ a %= 10;
+ printf("\nThe modular= operator is %d\n", a);
+
+ // += , -= , *= , %=, = these are examples of the assignment operators here
+
+
+return 0;
+}
